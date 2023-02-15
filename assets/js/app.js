00e309caaf240e421de76e481797e61dfad7f12b
axaoltl <101741429+axaoltl@users.noreply.github.com>
2023-02-15 17:20:10 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,6 @@ $('.popup_gallery').slick({
     prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"><ion-icon name="chevron-back-outline"></ion-icon></button>',
     nextArrow: '<button class="slick-next" aria-label="Next" type="button"><ion-icon name="chevron-forward-outline"></ion-icon></button>',
     cssEasing: 'cubic-bezier(0, 1.1, 0.15, 1)',
-    speed: 1,
+    speed: 500,
 });
 // easing: 'easeOutElastic'
