3e68df2a601eb3d975c92ca972982938b0e8d8ed
axaoltl <101741429+axaoltl@users.noreply.github.com>
2023-03-18 22:18:38 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,21 @@ iconLayerBox.forEach((icon, i) => {
         console.log(specIcon)
         const specIconCard = document.getElementById(`location-${specIcon}`)
         console.log(specIconCard)
+        console.log(specIconCard.scrollHeight)
         if(specIconCard.classList.contains('popup_active')){
             specIconCard.classList.remove('popup_active')
+            // specIconCard.setAttribute('style', `transform:translate(-200vw, -200vh)`)
         } else {
             specIconCard.classList.add('popup_active')
+            const cross = specIconCard.querySelector('::after')
+            console.log(cross)
+            // if (specIconCard.scrollHeight > 314){
+            //     // .popup.popup_side.popup_active
+            //     // console.log('needchange')
+            //     // console.log(specIconCard)
+            //     // console.log(specIconCard.scrollHeight)
+            //     specIconCard.setAttribute('style', `transform:translate(-360px, -${330+(specIconCard.scrollHeight-314)}px)`)
+            // }
         }
     })
 })
-
-
